41c6f4f9622d40e5b03ee51d2fa31ff38f00649d
Jonathan Dorety <kyullog@gmail.com>
2019-02-11 23:00:27 -0500

    Refactor list to use Note component

--- front-end-notepad/src/components/AllNotesList.js
+++ front-end-notepad/src/components/AllNotesList.js
@@ -1,15 +1,11 @@
 import React from "react";
+import Note from "./Note";
 
 const AllNotesList = props => {
   return (
     <div className="all wrapper">
       {props.notes.map(note => {
-        return (
-          <div className="note-thumb" key={note._id}>
-            <h3>{note.title}</h3>
-            <p>{note.textBody}</p>
-          </div>
-        );
+        return <Note {...note} key={note._id} />;
       })}
     </div>
   );
