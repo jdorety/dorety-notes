43c047a015160cef3984af8ffbe000bdc7a16546
Jonathan Dorety <kyullog@gmail.com>
2019-02-12 00:18:08 -0500

    Misc. styling to line up with mockup

--- front-end-notepad/src/components/AllNotesList.js
+++ front-end-notepad/src/components/AllNotesList.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Note from "./Note";
 
+import "../styles/AllNotesList.css";
+
 const AllNotesList = props => {
   return (
-    <div className="all wrapper">
-      {props.notes.map(note => {
-        return <Note {...note} key={note._id} />;
-      })}
+    <div className="notes-list">
+      <h2 className="list-header">Your Notes:</h2>
+      <div className="list-wrapper">
+        {props.notes.map(note => {
+          return <Note {...note} key={note._id} />;
+        })}
+      </div>
     </div>
   );
 };
--- front-end-notepad/src/components/Note.js
+++ front-end-notepad/src/components/Note.js
@@ -1,10 +1,13 @@
 import React from "react";
+import "../styles/Note.css";
 
 const Note = props => {
   return (
     <div className="note-thumb">
-      <h3>{props.title}</h3>
-      <p>{props.textBody}</p>
+      <div className="thumb-contents">
+        <h3 className="note-header">{props.title}</h3>
+        <p>{props.textBody}</p>
+      </div>
     </div>
   );
 };
