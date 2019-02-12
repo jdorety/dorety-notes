42993e030884a02b46cb2123818dad4e49d97eba
Jonathan Dorety <kyullog@gmail.com>
2019-02-11 22:53:46 -0500

    Add key prop

--- front-end-notepad/src/components/AllNotesList.js
+++ front-end-notepad/src/components/AllNotesList.js
@@ -5,7 +5,7 @@ const AllNotesList = props => {
     <div className="all wrapper">
       {props.notes.map(note => {
         return (
-          <div className="note-thumb">
+          <div className="note-thumb" key={note._id}>
             <h3>{note.title}</h3>
             <p>{note.textBody}</p>
           </div>
