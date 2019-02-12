e68baf772f6261d20226acf35a0f4e2fb012257d
Jonathan Dorety <kyullog@gmail.com>
2019-02-11 22:41:27 -0500

    Application gets notes from API and sets state with response data

--- front-end-notepad/src/App.js
+++ front-end-notepad/src/App.js
@@ -1,25 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      notes: []
+    };
+  }
+
+  getData = URL => {
+    axios
+      .get(URL)
+      .then(response =>
+        this.setState({
+          notes: response.data
+        })
+      )
+      .catch(err => console.log(err));
+  };
+
+  componentDidMount() {
+    this.getData(`https://fe-notes.herokuapp.com/note/get/all`);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Lambda Notes</h1>
       </div>
     );
   }
