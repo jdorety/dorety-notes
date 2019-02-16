0087a73eb4cd67e5e316e5e7df739fb99a9c6e37
Jonathan Dorety <kyullog@gmail.com>
2019-02-16 12:31:06 -0500

    Connect Redux store and reducer

---
+++ front-end-notepad/src/reducers/index.js
@@ -0,0 +1,10 @@
+const initialState = {
+  notes: "this worked"
+};
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
