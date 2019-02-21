0c572f1003ea96b9ac57653081cf55a506d56962
Jonathan Dorety <kyullog@gmail.com>
2019-02-20 20:24:25 -0500

    Add PropTypes to AddNoteButton

--- front-end-notepad/src/components/AddNoteButton.js
+++ front-end-notepad/src/components/AddNoteButton.js
@@ -1,15 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const AddNoteButton = props => {
   function clickHandler(e) {
     props.history.push("/addnote");
   }
-
   return (
     <button className="tool" onClick={clickHandler}>
       + Create Note
     </button>
   );
+};
+
+AddNoteButton.propTypes = {
+  history: PropTypes.object
 };
 
 export default AddNoteButton;
