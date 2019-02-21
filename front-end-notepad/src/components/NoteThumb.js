46c680716a2eff2305afd62510ea2512b021fe0f
Jonathan Dorety <kyullog@gmail.com>
2019-02-20 20:35:58 -0500

    Add PropTypes to NoteThumb

--- front-end-notepad/src/components/NoteThumb.js
+++ front-end-notepad/src/components/NoteThumb.js
@@ -1,11 +1,11 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "../styles/NoteThumb.css";
 
 const NoteThumb = props => {
   function clickHandler(e) {
     props.history.push(`/notes/${props._id}`);
   }
-
   return (
     <div className="note-thumb" onClick={clickHandler}>
       <div className="note-header-wrapper">
@@ -16,6 +16,19 @@ const NoteThumb = props => {
       </div>
     </div>
   );
+};
+
+NoteThumb.propTypes = {
+  getList: PropTypes.func,
+  history: PropTypes.object,
+  location: PropTypes.object,
+  match: PropTypes.object,
+  notes: PropTypes.array,
+  tags: PropTypes.array,
+  textBody: PropTypes.string,
+  title: PropTypes.string,
+  __v: PropTypes.number,
+  _id: PropTypes.string
 };
 
 export default NoteThumb;
