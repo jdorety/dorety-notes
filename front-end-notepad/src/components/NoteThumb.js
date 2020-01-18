fcaae404f37e59d24554a143519b1d6b0ba74eb1
Jonathan Dorety <kyullog@gmail.com>
2020-01-18 14:21:38 -0800

    Change notes thumbnail to Paper component

--- front-end-notepad/src/components/NoteThumb.js
+++ front-end-notepad/src/components/NoteThumb.js
@@ -1,28 +1,38 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
 import PropTypes from "prop-types";
 import "../styles/NoteThumb.css";
 
-const NoteThumb = props => {
-  const dispStr =
-    props.textBody.length > 55
-      ? props.textBody.slice(0, 54) + "..."
-      : props.textBody;
+const useStyles = makeStyles(theme => ({
+  paper: {
+    height: 200,
+    width: 200
+  }
+}))
 
-  const dispTitle =
-    props.title.length > 35 ? props.title.slice(0, 34) + "..." : props.title;
+const NoteThumb = props => {
+  const classes= useStyles();
+  // const dispStr =
+  //   props.textBody.length > 55
+  //     ? props.textBody.slice(0, 54) + "..."
+  //     : props.textBody;
+
+  // const dispTitle =
+  //   props.title.length > 35 ? props.title.slice(0, 34) + "..." : props.title;
 
   function clickHandler(e) {
     props.history.push(`/notes/${props._id}`);
   }
   return (
-    <div className="note-thumb" onClick={clickHandler}>
+    <Paper className={classes.paper} onClick={clickHandler}>
       <div className="note-header-wrapper">
-        <h3 className="note-header">{dispTitle}</h3>
+        <h3 className="note-header">{props.title}</h3>
       </div>
       <div className="thumb-contents">
-        <p>{dispStr}</p>
+        <p>{props.textBody}</p>
       </div>
-    </div>
+    </Paper>
   );
 };
 
