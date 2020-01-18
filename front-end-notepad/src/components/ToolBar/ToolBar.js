c44b47605759580f47e217680e927a9091aa53f6
Jonathan Dorety <kyullog@gmail.com>
2020-01-18 15:42:59 -0800

    Refactor Toolbar

--- front-end-notepad/src/components/ToolBar/ToolBar.js
+++ front-end-notepad/src/components/ToolBar/ToolBar.js
@@ -1,21 +1,45 @@
 import React from "react";
 import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import ToolBar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 import AddNoteButton from "./buttons/AddNoteButton";
 import HomeButton from "./buttons/HomeButton";
 
 import "../../styles/ToolBar.css";
+import { makeStyles } from "@material-ui/core";
 
-const ToolBar = props => {
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
+
+const TopBar = props => {
+  const classes = useStyles();
+
   return (
-    <header className="toolbar">
-      <nav>
-        <h1 id="nav-logo">Dorety Notes</h1>
-        <div className="button-container">
-          <AddNoteButton history={props.history} />
-          <HomeButton history={props.history} />
-        </div>
-      </nav>
-    </header>
+    // <header className="toolbar">
+    //   <nav>
+    //     <h1 id="nav-logo">Dorety Notes</h1>
+    //     <div className="button-container">
+    //       <AddNoteButton history={props.history} />
+    //       <HomeButton history={props.history} />
+    //     </div>
+    //   </nav>
+    // </header>
+    <AppBar position="sticky">
+      <ToolBar>
+        <Typography variant="h3" className={classes.title}>
+          Dorety Notes
+        </Typography>
+        <AddNoteButton />
+        <HomeButton />
+      </ToolBar>
+    </AppBar>
   );
 };
 
@@ -25,4 +49,4 @@ ToolBar.propTypes = {
   match: PropTypes.object
 };
 
-export default ToolBar;
+export default TopBar;
