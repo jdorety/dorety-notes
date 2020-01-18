2ac46e50a684489336248e591117340d6aa4f990
Jonathan Dorety <kyullog@gmail.com>
2020-01-18 15:12:36 -0800

    Refactor thumbnail for MUI.  Overflow needs fixing

--- front-end-notepad/src/components/NoteThumb.js
+++ front-end-notepad/src/components/NoteThumb.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 import Paper from "@material-ui/core/Paper";
 import PropTypes from "prop-types";
 import "../styles/NoteThumb.css";
@@ -7,12 +9,14 @@ import "../styles/NoteThumb.css";
 const useStyles = makeStyles(theme => ({
   paper: {
     height: 200,
-    width: 200
+    width: 200,
+    overflow: "hidden",
+    textOverflow: "ellipses"
   }
-}))
+}));
 
 const NoteThumb = props => {
-  const classes= useStyles();
+  const classes = useStyles();
   // const dispStr =
   //   props.textBody.length > 55
   //     ? props.textBody.slice(0, 54) + "..."
@@ -21,18 +25,32 @@ const NoteThumb = props => {
   // const dispTitle =
   //   props.title.length > 35 ? props.title.slice(0, 34) + "..." : props.title;
 
-  function clickHandler(e) {
+  function clickHandler() {
     props.history.push(`/notes/${props._id}`);
   }
   return (
+    <Typography component="div">
     <Paper className={classes.paper} onClick={clickHandler}>
-      <div className="note-header-wrapper">
-        <h3 className="note-header">{props.title}</h3>
-      </div>
-      <div className="thumb-contents">
-        <p>{props.textBody}</p>
-      </div>
+          <Box
+            textAlign="left"
+            fontWeight={800}
+            fontSize={20}
+            m={1}
+            component="div"
+          >
+            {props.title}
+          </Box>
+          <Box
+            className={classes.paper}
+            component="div"
+            textAlign="left"
+            fontSize={14}
+            m={0.5}
+          >
+            {props.textBody}
+          </Box>
     </Paper>
+      </Typography>
   );
 };
 
