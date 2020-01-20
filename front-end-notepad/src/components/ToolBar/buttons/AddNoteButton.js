c7c41c0e04530fd6d25301f541b3376881880762
Jonathan Dorety <kyullog@gmail.com>
2020-01-19 20:43:03 -0800

    Refactor Add button to MUI

--- front-end-notepad/src/components/ToolBar/buttons/AddNoteButton.js
+++ front-end-notepad/src/components/ToolBar/buttons/AddNoteButton.js
@@ -1,17 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { ReactComponent as Add } from "../../../icons/plus.svg";
+import Button from "@material-ui/core/Button";
+import Icon from "@material-ui/core/Icon";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1)
+    }
+  }
+}));
 
 const AddNoteButton = props => {
+  const classes = useStyles();
+
   //Pushing button routes to add note page
   function clickHandler(e) {
     props.history.push("/addnote");
   }
   return (
-    <button className="tool" onClick={clickHandler}>
-      <span className="desk-tool">Add Note +</span>
-      <Add className="mobile-tool" />
-    </button>
+    <div className={classes.root}>
+      <Button
+        className="tool"
+        startIcon={<Icon>add</Icon>}
+        variant="contained"
+        onClick={clickHandler}
+      >
+        Add Note
+      </Button>
+    </div>
   );
 };
 
